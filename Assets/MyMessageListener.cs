fc836edec51bf109ba5bbf18724e2f845352ae98
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-07-25 13:13:41 +1200

    disable debug zapping

--- Assets/MyMessageListener.cs
+++ Assets/MyMessageListener.cs
@@ -59,10 +59,6 @@ public class MyMessageListener : MonoBehaviour {
     }
     // Update is called once per frame
     void Update () {
-        if (Input.GetAxis("Vertical") > 0.0F) {
-            Debug.Log("Sending zaaap");
-            serialController.SendSerialMessage("zaaap\r");
-        }
     }
     // Invoked when a line of data is received from the serial device.
     void OnMessageArrived(string msg)
